fa7f3a57399bafd7e3f7b008058edd7218c2db47
Diana Agapkina <43079357+agapkinadiana@users.noreply.github.com>
2020-11-18 13:56:51 +0300

    add: Setup Locations TableView

--- Uber/Controller/HomeController.swift
+++ Uber/Controller/HomeController.swift
@@ -9,6 +9,8 @@ import UIKit
 import Firebase
 import MapKit
 
+private let reuseIdentifier = "LocationCell"
+
 class HomeController: UIViewController {
     
     //MARK: - Properties
@@ -18,6 +20,9 @@ class HomeController: UIViewController {
     
     private let inputActivationView = LocationInputActivationView()
     private let locationInputView = LocationInputView()
+    private let tableView = UITableView()
+    
+    private final let locationInputViewHeight: CGFloat = 200
     
     // MARK: - Lifecycle
     
@@ -78,7 +83,7 @@ class HomeController: UIViewController {
     func configureLocationInputView() {
         view.addSubview(locationInputView)
         locationInputView.anchor(top: view.topAnchor, left: view.leftAnchor,
-                                 right: view.rightAnchor, height: 200)
+                                 right: view.rightAnchor, height: locationInputViewHeight)
         locationInputView.alpha = 0
         locationInputView.delegate = self
         
@@ -87,6 +92,20 @@ class HomeController: UIViewController {
         } completion: { _ in
             print("DEBUG: Present table view..")
         }
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(LocationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        
+        let height = view.frame.height - locationInputViewHeight
+        tableView.frame = CGRect(x: 0, y: view.frame.height,
+                                 width: view.frame.width, height: height)
+        
+        view.addSubview(tableView)
     }
 }
 
@@ -143,5 +162,18 @@ extension HomeController: LocationInputViewDelegate {
                 self.inputActivationView.alpha = 1
             }
         }
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! LocationCell
+        return cell
     }
 }
---
+++ Uber/View/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  Uber
+//
+//  Created by Diana Agapkina on 18.11.20.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
