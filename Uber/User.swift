c336e5636b0e56fbbb7ee8c1ff30206f8bda617f
Diana Agapkina <43079357+agapkinadiana@users.noreply.github.com>
2020-11-20 14:00:54 +0300

    add: Fetching Nearby Drivers From Database

--- Uber/User.swift
+++ Uber/User.swift
@@ -5,10 +5,13 @@
 //  Created by Diana Agapkina on 18.11.20.
 //
 
+import CoreLocation
+
 struct User {
     let fullname: String
     let email: String
     let accountType: Int
+    var location: CLLocation?
     
     init(dictionary: [String: Any]) {
         self.fullname = dictionary["fullname"] as? String ?? ""
